51d0f8286c3dd800c0a29f83e8d406dfcb32fea2
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-11-23 19:07:06 +0000

    #1 - Enforce type check on route props

--- src/screens/MapModule/MapController.tsx
+++ src/screens/MapModule/MapController.tsx
@@ -1,11 +1,18 @@
 import React, {FunctionComponent} from 'react';
 import {MapScreen} from './MapScreen';
+import {RouteProp} from '@react-navigation/native';
 
-type RouteData = {
-  route: any;
+type RootStackParamList = {
+  Map: {mapAddress: string};
 };
 
-const MapController: FunctionComponent<RouteData> = ({route}) => {
+type MapScreenRouteProp = RouteProp<RootStackParamList, 'Map'>;
+
+type Props = {
+  route: MapScreenRouteProp;
+};
+
+const MapController: FunctionComponent<Props> = ({route}) => {
   const {mapAddress} = route.params;
 
   return <MapScreen mapAddress={mapAddress} />;
